2502e363190a5588e0731347c8d1b358167cefa7
SerhiiKutovyi <wagenrex@gmail.com>
2023-02-05 16:42:23 +0200

    Details

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,4 +1,5 @@
 import { useFetchMovieDetails } from 'hooks/FetchMovieDetails';
+import { Details } from './MovieDetails.styleds';
 
 export const MovieDetails = () => {
   const details = useFetchMovieDetails();
@@ -6,21 +7,24 @@ export const MovieDetails = () => {
   return (
     <>
       {details && (
-        <div>
-          <img
-            src={`https://image.tmdb.org/t/p/w200/${details.poster_path}`}
-            alt="ert"
-          />
-          <h2>
-            {details.title} ({details.release_date})
-          </h2>
-
-          <p>User score: {details.popularity}</p>
-          <h3>Owerview</h3>
-          <p>{details.overview}</p>
-          <h3>Genres</h3>
-          <p>{details.genres.name}</p>
-        </div>
+        <Details>
+          <div>
+            <img
+              src={`https://image.tmdb.org/t/p/w300/${details.poster_path}`}
+              alt="ert"
+            />
+          </div>
+          <div>
+            <h2>
+              {details.title} ({details.release_date})
+            </h2>
+            <p>User score: {details.popularity}</p>
+            <h3>Owerview</h3>
+            <p>{details.overview}</p>
+            <h3>Genres</h3>
+            <p>{details.genres.name}</p>
+          </div>
+        </Details>
       )}
     </>
   );
